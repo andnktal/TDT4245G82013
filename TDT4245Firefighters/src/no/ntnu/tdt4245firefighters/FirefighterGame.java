bf1539a1291bff90db2c75f5e9f44bb4ca902c50
pawanchamling <pawanchamling.kirat@gmail.com>
2013-11-08 12:48:59 +0100

    Added timer.

--- TDT4245Firefighters/src/no/ntnu/tdt4245firefighters/FirefighterGame.java
+++ TDT4245Firefighters/src/no/ntnu/tdt4245firefighters/FirefighterGame.java
@@ -1,16 +1,22 @@
 package no.ntnu.tdt4245firefighters;
 
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.RectF;
-import android.util.Log;
-import android.view.MotionEvent;
 import sheep.game.State;
+import sheep.graphics.Font;
 import sheep.graphics.Image;
 import sheep.gui.TextButton;
 import sheep.gui.WidgetAction;
 import sheep.gui.WidgetListener;
 import sheep.input.TouchListener;
+import android.content.pm.LabeledIntent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class FirefighterGame extends State implements WidgetListener{
 
@@ -27,7 +33,22 @@ public class FirefighterGame extends State implements WidgetListener{
 	
 	private Matrix matrix;
 	
-	private TextButton test;
+	private TextButton msgBox;
+	private TextButton timer;
+	private String timeValue;
+	
+	//==|| For the timerline
+	private int countDownMaxSeconds = 30; // 30 seconds
+	private int maxTimeInMilliseconds = countDownMaxSeconds * 1000;
+	private int countDownUnitInSeconds = 1; //1 seconds
+	private int countDownUnitInMilliseconds = countDownUnitInSeconds * 1000; 
+	private int timerBarLength = 0;
+	private int eachUnitLength = timerBarLength / countDownMaxSeconds;
+	
+	private Paint timerBar = new Paint();
+	private Rect timerBarRectangle; //
+	
+	private int screenWidthIs;
 	
 	public FirefighterGame(int screenWidth, int screenHeight)
 	{
@@ -40,6 +61,14 @@ public class FirefighterGame extends State implements WidgetListener{
 		matrix.setRectToRect(image, screen, Matrix.ScaleToFit.CENTER);
 		matrix.setRotate(90, screenWidth/2, (screenWidth/2));
 		
+		timeValue = "15:00";
+	
+		
+		timer = new TextButton(screenWidth - 50, 30, timeValue, new Paint[]{Font.BLUE_SANS_BOLD_20, Font.BLUE_SANS_BOLD_20});
+		
+		//msgBox = new TextButton(screenWidth - 70, 30, "something", new Paint[]{Font.BLUE_SANS_BOLD_20, Font.BLUE_SANS_BOLD_20});
+		//timer.
+		
 		event1 = new Image(R.drawable.baby);
 		event2 = new Image(R.drawable.baby);
 		event3 = new Image(R.drawable.baby);
@@ -47,6 +76,8 @@ public class FirefighterGame extends State implements WidgetListener{
 		p1pos = new Image(R.drawable.ff_pos1);
 		fP1X = 50.0f;
 		fP1Y = screenHeight - p1pos.getHeight();
+		
+		timerBarRectangle =  new Rect(10, 10, screenWidth - 10 , 30);
 		
 		TouchListener touchMove = new TouchListener(){
 
@@ -72,6 +103,45 @@ public class FirefighterGame extends State implements WidgetListener{
 			
 		};
 		
+		
+		screenWidthIs = screenWidth;
+		timerBarLength = screenWidth - 10;
+		eachUnitLength = timerBarLength / countDownMaxSeconds;
+		
+		//==|| The Timer
+		new CountDownTimer(maxTimeInMilliseconds, countDownUnitInMilliseconds) {
+
+		     public void onTick(long millisUntilFinished) {		    	 
+		    	 String val = Long.toString(millisUntilFinished / 1000);
+		    	 timer.setLabel(val);
+		    	 timerBarLength = timerBarLength - eachUnitLength;
+		    	// msgBox.setLabel(Integer.toString(timerBarLength));
+		    	 
+		    	 //Log.i("screenWidthIs", Integer.toString(screenWidthIs) + " , timerBarLength = " + Integer.toString(timerBarLength) + " , percentage = "+ Float.toString((float)timerBarLength / (float)(screenWidthIs - 20)));
+		    	 
+		    	 timerBarRectangle.set(10, 10, timerBarLength, 30);
+		    	 
+		    	 if((float)timerBarLength /(float) (screenWidthIs - 20) * 100 > 75 ){
+		    		 timerBar.setColor(Color.GREEN);
+		    	 }
+		    	 else if((float)timerBarLength /(float) (screenWidthIs - 20) * 100 > 50 ){
+		    		 timerBar.setColor(Color.BLUE);
+		    	 }
+		    	 else if((float)timerBarLength /(float) (screenWidthIs - 20) * 100 > 25 ){
+		    		 timerBar.setColor(Color.YELLOW);
+		    	 }
+		    	 else {
+		    		 timerBar.setColor(Color.RED);
+		    	 }
+		    	 
+		     }
+
+		     public void onFinish() {
+		    	 timer.setLabel("0");
+		     }
+		  }.start();
+		 
+		
 		this.addTouchListener(touchMove);
 	}
 	
@@ -84,6 +154,11 @@ public class FirefighterGame extends State implements WidgetListener{
 		event2.draw(canvas, 184.5f, 371.5f);
 		event3.draw(canvas, 306.5f, 593.5f);
 		p1pos.draw(canvas, fP1X, fP1Y);
+		
+		timer.draw(canvas);
+		//msgBox.draw(canvas);
+		
+		canvas.drawRect(timerBarRectangle, timerBar); //drawLine(10, 10, timerBarLength, 10, timerBar);
 	}	
 	
 	@Override
